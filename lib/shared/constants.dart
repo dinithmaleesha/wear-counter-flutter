54c2816300b07710777775d8c1e93c7bae7104e5
Dinith Maleesha <maleesha.live123@gmail.com>
2024-05-10 23:56:16 +0530

    Create textInputDecoration

---
+++ lib/shared/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const textInputDecoration = InputDecoration(
+  fillColor: Colors.white,
+  filled: true,
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.white, width: 2.0),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color.fromRGBO(63, 81, 181, 1), width: 2.0),
+  ),
+);
